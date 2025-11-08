0b07773af9be5210a9ecf75b749af2894bf3be11
Souvik Chakraborty <Souvikc3030@gmail.com>
2025-11-08 16:16:59 +0530

    sqlx and docker commands

---
+++ cmds.sh
@@ -0,0 +1,12 @@
+sqlx command
+sqlx command
+sqlx migrate add -r init
+sqlx migrate run
+sqlx migrate revert
+#
+# Docker Command
+docker tag local-image:tagname new-repo:tagname
+docker push new-repo:tagname
+# or build the image with the same name of the repo
+# then use the push command with the name of the repo
+
